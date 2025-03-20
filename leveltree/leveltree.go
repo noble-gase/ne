93779bd8f52fb910a0eab8b13cc3a7141f8b47c6
盛辉 <hui.sheng@sunmi.com>
2025-03-20 09:53:51 +0800

    Update leveltree

--- leveltree/leveltree.go
+++ leveltree/leveltree.go
@@ -2,27 +2,42 @@ package leveltree
 
 // LevelNode 层级树泛型约束
 type LevelNode[E comparable] interface {
-	GetID() E
-	GetPid() E
+	GetId() E
+	GetPId() E
 }
 
-// LevelTree 菜单或分类层级树
+// LevelTree 菜单和组织单位等分类层级树
 type LevelTree[T LevelNode[E], E comparable] struct {
-	Data     T
-	Children []*LevelTree[T, E]
+	Data     T                  `json:"data"`
+	Children []*LevelTree[T, E] `json:"children"`
 }
 
-// New 构建菜单或分类层级树（data=按pid归类后的数据, pid=树的起始ID）
-func New[T LevelNode[E], E comparable](data map[E][]T, pid E) []*LevelTree[T, E] {
-	nodes := data[pid]
+func buildTree[T LevelNode[E], E comparable](classify map[E][]T, rootId E) []*LevelTree[T, E] {
+	nodes := classify[rootId]
 	count := len(nodes)
 	root := make([]*LevelTree[T, E], 0, count)
 	for i := 0; i < count; i++ {
 		node := nodes[i]
 		root = append(root, &LevelTree[T, E]{
 			Data:     node,
-			Children: New(data, node.GetID()),
+			Children: buildTree(classify, node.GetId()),
 		})
 	}
 	return root
+}
+
+// New 构建菜单和组织单位等分类层级树
+//
+//	data = 一组带有pid的数据
+//	rootId = 树的起始ID
+func New[T LevelNode[E], E comparable](data []T, rootId E) []*LevelTree[T, E] {
+	classify := make(map[E][]T, 0)
+	for _, v := range data {
+		pid := v.GetPId()
+		if _, ok := classify[pid]; !ok {
+			classify[pid] = make([]T, 0)
+		}
+		classify[pid] = append(classify[pid], v)
+	}
+	return buildTree(classify, rootId)
 }
--- leveltree/leveltree_test.go
+++ leveltree/leveltree_test.go
@@ -11,75 +11,65 @@ type Foo struct {
 	Name string `json:"name"`
 }
 
-func (f *Foo) GetID() int {
+func (f *Foo) GetId() int {
 	return f.ID
 }
 
-func (f *Foo) GetPid() int {
+func (f *Foo) GetPId() int {
 	return f.Pid
 }
 
 func TestTreeInt(t *testing.T) {
-	data := map[int][]*Foo{
-		0: {
-			{
-				ID:   1,
-				Pid:  0,
-				Name: "foo-1",
-			},
-			{
-				ID:   2,
-				Pid:  0,
-				Name: "foo-2",
-			},
+	data := []*Foo{
+		{
+			ID:   1,
+			Pid:  0,
+			Name: "foo-1",
 		},
-		1: {
-			{
-				ID:   3,
-				Pid:  1,
-				Name: "foo-3",
-			},
-			{
-				ID:   4,
-				Pid:  1,
-				Name: "foo-4",
-			},
+		{
+			ID:   2,
+			Pid:  0,
+			Name: "foo-2",
 		},
-		2: {
-			{
-				ID:   5,
-				Pid:  2,
-				Name: "foo-5",
-			},
-			{
-				ID:   6,
-				Pid:  2,
-				Name: "foo-6",
-			},
+		{
+			ID:   3,
+			Pid:  1,
+			Name: "foo-3",
 		},
-		3: {
-			{
-				ID:   7,
-				Pid:  3,
-				Name: "foo-7",
-			},
-			{
-				ID:   8,
-				Pid:  3,
-				Name: "foo-8",
-			},
+		{
+			ID:   4,
+			Pid:  1,
+			Name: "foo-4",
 		},
-		4: {
-			{
-				ID:   9,
-				Pid:  4,
-				Name: "foo-9",
-			},
-			{
-				ID:   10,
-				Pid:  4,
-				Name: "foo-10",
-			},
+		{
+			ID:   5,
+			Pid:  2,
+			Name: "foo-5",
+		},
+		{
+			ID:   6,
+			Pid:  2,
+			Name: "foo-6",
+		},
+		{
+			ID:   7,
+			Pid:  3,
+			Name: "foo-7",
+		},
+		{
+			ID:   8,
+			Pid:  3,
+			Name: "foo-8",
+		},
+		{
+			ID:   9,
+			Pid:  4,
+			Name: "foo-9",
+		},
+		{
+			ID:   10,
+			Pid:  4,
+			Name: "foo-10",
 		},
 	}
 
@@ -94,75 +84,65 @@ type Bar struct {
 	Name string `json:"name"`
 }
 
-func (b *Bar) GetID() string {
+func (b *Bar) GetId() string {
 	return b.ID
 }
 
-func (b *Bar) GetPid() string {
+func (b *Bar) GetPId() string {
 	return b.Pid
 }
 
 func TestTreeStr(t *testing.T) {
-	data := map[string][]*Bar{
-		"0": {
-			{
-				ID:   "1",
-				Pid:  "0",
-				Name: "bar-1",
-			},
-			{
-				ID:   "2",
-				Pid:  "0",
-				Name: "bar-2",
-			},
+	data := []*Bar{
+		{
+			ID:   "1",
+			Pid:  "0",
+			Name: "bar-1",
 		},
-		"1": {
-			{
-				ID:   "3",
-				Pid:  "1",
-				Name: "bar-3",
-			},
-			{
-				ID:   "4",
-				Pid:  "1",
-				Name: "bar-4",
-			},
+		{
+			ID:   "2",
+			Pid:  "0",
+			Name: "bar-2",
 		},
-		"2": {
-			{
-				ID:   "5",
-				Pid:  "2",
-				Name: "bar-5",
-			},
-			{
-				ID:   "6",
-				Pid:  "2",
-				Name: "bar-6",
-			},
+		{
+			ID:   "3",
+			Pid:  "1",
+			Name: "bar-3",
 		},
-		"3": {
-			{
-				ID:   "7",
-				Pid:  "3",
-				Name: "bar-7",
-			},
-			{
-				ID:   "8",
-				Pid:  "3",
-				Name: "bar-8",
-			},
+		{
+			ID:   "4",
+			Pid:  "1",
+			Name: "bar-4",
 		},
-		"4": {
-			{
-				ID:   "9",
-				Pid:  "4",
-				Name: "bar-9",
-			},
-			{
-				ID:   "10",
-				Pid:  "4",
-				Name: "bar-10",
-			},
+		{
+			ID:   "5",
+			Pid:  "2",
+			Name: "bar-5",
+		},
+		{
+			ID:   "6",
+			Pid:  "2",
+			Name: "bar-6",
+		},
+		{
+			ID:   "7",
+			Pid:  "3",
+			Name: "bar-7",
+		},
+		{
+			ID:   "8",
+			Pid:  "3",
+			Name: "bar-8",
+		},
+		{
+			ID:   "9",
+			Pid:  "4",
+			Name: "bar-9",
+		},
+		{
+			ID:   "10",
+			Pid:  "4",
+			Name: "bar-10",
 		},
 	}
 

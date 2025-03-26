33a4f31d8ec9a724678993dd3d2f4c2c4305d70f
盛辉 <hui.sheng@sunmi.com>
2025-03-26 09:29:15 +0800

    Update README.md

--- time_test.go
+++ time_test.go
@@ -7,18 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDate(t *testing.T) {
-	tz, err := time.LoadLocation("Asia/Shanghai")
-	assert.NoError(t, err)
-	assert.Equal(t, "2016-03-19 15:03:19", TimeToStr(time.DateTime, 1458370999, tz))
-}
-
-func TestStrToTime(t *testing.T) {
-	tz, err := time.LoadLocation("Asia/Shanghai")
-	assert.NoError(t, err)
-	assert.Equal(t, int64(1562910319), StrToTime(time.DateTime, "2019-07-12 13:45:19", tz).Unix())
-}
-
 func TestWeekAround(t *testing.T) {
 	tz, err := time.LoadLocation("Asia/Shanghai")
 	assert.NoError(t, err)

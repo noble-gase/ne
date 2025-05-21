3595a4bd5874d70974bc47e00fb669951e94bac7
盛辉 <hui.sheng@sunmi.com>
2025-05-21 15:17:16 +0800

    Update transaction

--- helper/db.go
+++ helper/db.go
@@ -12,6 +12,11 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type (
+	DB = map[string]*sql.DB
+	TX = map[string]*sql.Tx
+)
+
 // DBConfig 数据库初始化配置
 type DBConfig struct {
 	// Driver 驱动名称
@@ -51,34 +56,89 @@ func NewDB(cfg *DBConfig) (*sql.DB, error) {
 	return db, nil
 }
 
-// Transaction 执行数据库事物
-func Transaction(ctx context.Context, db *sql.DB, fn func(ctx context.Context, tx *sql.Tx) error) (err error) {
-	tx, _err := db.BeginTx(ctx, nil)
+// Transaction 执行数据库事务
+func Transaction(ctx context.Context, db *sql.DB, fn func(ctx context.Context, tx *sql.Tx) error, opts ...*sql.TxOptions) (err error) {
+	var opt *sql.TxOptions
+	if len(opts) != 0 {
+		opt = opts[0]
+	}
+
+	tx, _err := db.BeginTx(ctx, opt)
 	if _err != nil {
 		err = fmt.Errorf("begin transaction: %w", _err)
 		return
 	}
 
+	rollback := func(err error) error {
+		if e := tx.Rollback(); e != nil {
+			err = fmt.Errorf("%w: rollback: %w", err, e)
+		}
+		return err
+	}
+
 	defer func() {
 		if r := recover(); r != nil {
-			err = fmt.Errorf("transaction panic recovered: %+v", r)
-			if err_ := tx.Rollback(); err_ != nil {
-				err = fmt.Errorf("%w: rollback: %w", err, err_)
-			}
-			err = fmt.Errorf("%w\n%s", err, string(debug.Stack()))
+			// if panic, should rollback
+			e := fmt.Errorf("transaction panic recovered: %+v", r)
+			err = fmt.Errorf("%w\n%s", rollback(e), string(debug.Stack()))
 		}
 	}()
 
-	if err = fn(ctx, tx); err != nil {
-		if err_ := tx.Rollback(); err_ != nil {
-			err = fmt.Errorf("%w: transaction rollback: %w", err, err_)
-		}
+	if e := fn(ctx, tx); e != nil {
+		err = rollback(e)
 		return
 	}
 
-	if _err = tx.Commit(); _err != nil {
-		err = fmt.Errorf("transaction commit: %w", _err)
+	if e := tx.Commit(); e != nil {
+		err = fmt.Errorf("commit: %w", e)
+	}
+	return
+}
+
+// TransactionX 执行多数据库事务
+func TransactionX(ctx context.Context, db DB, fn func(ctx context.Context, tx TX) error, opts ...*sql.TxOptions) (err error) {
+	var opt *sql.TxOptions
+	if len(opts) != 0 {
+		opt = opts[0]
+	}
+
+	tx := make(TX, len(db))
+	for k, v := range db {
+		x, e := v.BeginTx(ctx, opt)
+		if e != nil {
+			err = fmt.Errorf("begin transaction (%s): %w", k, e)
+			return
+		}
+		tx[k] = x
+	}
+
+	rollback := func(err error) error {
+		for k, v := range tx {
+			if e := v.Rollback(); e != nil {
+				err = fmt.Errorf("%w; %s.Rollback: %w", err, k, e)
+			}
+		}
+		return err
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			// if panic, should rollback
+			e := fmt.Errorf("transaction panic recovered: %+v", r)
+			err = fmt.Errorf("%w\n%s", rollback(e), string(debug.Stack()))
+		}
+	}()
+
+	if e := fn(ctx, tx); e != nil {
+		err = rollback(e)
 		return
+	}
+
+	for k, v := range tx {
+		if e := v.Commit(); e != nil {
+			err = rollback(fmt.Errorf("%s.Commit: %w", k, e))
+			return
+		}
 	}
 	return
 }

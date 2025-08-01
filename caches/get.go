dc17315c2ce19297a8bd211f6ce0ccaeb4684f52
盛辉 <hui.sheng@sunmi.com>
2025-08-01 18:02:33 +0800

    Update caches

--- caches/get.go
+++ caches/get.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"log/slog"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
-func Get[T any](ctx context.Context, cli redis.UniversalClient, key string, fn func(ctx context.Context) (T, error), ttl time.Duration) (T, error) {
+func Get[T any](ctx context.Context, cli redis.UniversalClient, key string, fn func(ctx context.Context) (T, bool, error), ttl time.Duration) (T, error) {
 	var ret T
 
 	if cli == nil {
@@ -19,7 +21,7 @@ func Get[T any](ctx context.Context, cli redis.UniversalClient, key string, fn f
 	str, err := cli.Get(ctx, key).Result()
 	if err == nil {
 		_err := json.Unmarshal([]byte(str), &ret)
-		return ret, _err
+		return ret, fmt.Errorf("unmarshal: %w", _err)
 	}
 	if !errors.Is(err, redis.Nil) {
 		return ret, err
@@ -28,14 +30,23 @@ func Get[T any](ctx context.Context, cli redis.UniversalClient, key string, fn f
 	// 缓存未命中
 	data, err, _ := sf.Do(key, func() (any, error) {
 		// 调用fn获取数据
-		data, _err := fn(ctx)
+		data, ok, _err := fn(ctx)
 		if _err != nil {
 			sf.Forget(key)
 			return nil, _err
 		}
+		if !ok {
+			return data, nil
+		}
+
 		// 缓存数据
-		if b, _err := json.Marshal(data); _err == nil {
-			cli.Set(ctx, key, string(b), ttl)
+		b, _err := json.Marshal(data)
+		if _err != nil {
+			slog.ErrorContext(ctx, "[caches:Get] marshal data failed", slog.String("key", key), slog.String("err", _err.Error()))
+			return data, nil
+		}
+		if _err = cli.Set(ctx, key, string(b), ttl).Err(); _err != nil {
+			slog.ErrorContext(ctx, "[caches:Get] set data failed", slog.String("key", key), slog.String("value", string(b)), slog.String("err", _err.Error()))
 		}
 		return data, nil
 	})
--- caches/hget.go
+++ caches/hget.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"log/slog"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
-func HGet[T any](ctx context.Context, cli redis.UniversalClient, key, field string, fn func(ctx context.Context) (T, error), ttl time.Duration) (T, error) {
+func HGet[T any](ctx context.Context, cli redis.UniversalClient, key, field string, fn func(ctx context.Context) (T, bool, error), ttl time.Duration) (T, error) {
 	var ret T
 
 	if cli == nil {
@@ -19,7 +21,7 @@ func HGet[T any](ctx context.Context, cli redis.UniversalClient, key, field stri
 	str, err := cli.HGet(ctx, key, field).Result()
 	if err == nil {
 		_err := json.Unmarshal([]byte(str), &ret)
-		return ret, _err
+		return ret, fmt.Errorf("unmarshal: %w", _err)
 	}
 	if !errors.Is(err, redis.Nil) {
 		return ret, err
@@ -29,17 +31,27 @@ func HGet[T any](ctx context.Context, cli redis.UniversalClient, key, field stri
 	sfKey := key + ":" + field
 	data, err, _ := sf.Do(sfKey, func() (any, error) {
 		// 调用fn获取数据
-		data, _err := fn(ctx)
+		data, ok, _err := fn(ctx)
 		if _err != nil {
 			sf.Forget(sfKey)
 			return nil, _err
 		}
+		if !ok {
+			return data, nil
+		}
+
 		// 缓存数据
-		if b, _err := json.Marshal(data); _err == nil {
-			cli.HSet(ctx, key, field, string(b))
-			if ttl > 0 && cli.TTL(ctx, key).Val() == -1 {
-				cli.Expire(ctx, key, ttl)
-			}
+		b, _err := json.Marshal(data)
+		if _err != nil {
+			slog.ErrorContext(ctx, "[caches:HGet] marshal data failed", slog.String("key", key), slog.String("field", field), slog.String("err", _err.Error()))
+			return data, nil
+		}
+		if _err = cli.HSet(ctx, key, field, string(b)).Err(); _err != nil {
+			slog.ErrorContext(ctx, "[caches:HGet] hset data failed", slog.String("key", key), slog.String("field", field), slog.String("value", string(b)), slog.String("err", _err.Error()))
+			return data, nil
+		}
+		if ttl > 0 && cli.TTL(ctx, key).Val() == -1 {
+			cli.Expire(ctx, key, ttl)
 		}
 		return data, nil
 	})

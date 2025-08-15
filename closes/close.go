169871a8207747ff49594854bbf3e53803b481bf
盛辉 <hui.sheng@sunmi.com>
2025-08-15 10:30:52 +0800

    Update closes

--- closes/close.go
+++ closes/close.go
@@ -8,9 +8,9 @@ import (
 )
 
 type closer struct {
-	name     string
-	priority int
-	fn       func() error
+	id string
+	px Priority
+	fn func() error
 }
 
 var (
@@ -18,26 +18,30 @@ var (
 	mutex   sync.Mutex
 )
 
-func Add(name string, priority int, fn func() error) {
+// Add 将资源关闭操作添加到关闭队列中
+//
+//	按 [P0 - P100] 顺序关闭（相同优先级按添加顺序关闭）
+func Add(id string, px Priority, fn func() error) {
 	mutex.Lock()
 	defer mutex.Unlock()
 
 	closers = append(closers, closer{
-		name:     name,
-		priority: priority,
-		fn:       fn,
+		id: id,
+		px: px,
+		fn: fn,
 	})
 }
 
+// Close 关闭队列中的资源
 func Close() {
-	sort.Slice(closers, func(i, j int) bool {
-		return closers[i].priority < closers[j].priority
+	sort.SliceStable(closers, func(i, j int) bool {
+		return closers[i].px < closers[j].px
 	})
 
 	for _, v := range closers {
-		fmt.Println("close", v.name, "...")
+		fmt.Println("close", v.id, "...")
 		if err := v.fn(); err != nil {
-			slog.Error("close "+v.name+" failed", "error", err)
+			slog.Error("close "+v.id+" failed", "error", err)
 		}
 	}
 }

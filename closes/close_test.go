6c63648c110ed5b65a048a6e80a398074646e4c1
盛辉 <hui.sheng@sunmi.com>
2025-08-15 10:32:21 +0800

    Update closes

--- closes/close_test.go
+++ closes/close_test.go
@@ -13,21 +13,21 @@ func TestClose(t *testing.T) {
 	wg.Add(3)
 	go func() {
 		defer wg.Done()
-		Add("test-1", 1, func() error {
+		Add("test-1", P1, func() error {
 			fmt.Println("test-1 bye-bye")
 			return nil
 		})
 	}()
 	go func() {
 		defer wg.Done()
-		Add("test-3", 3, func() error {
+		Add("test-3", P3, func() error {
 			fmt.Println("test-3 bye-bye")
 			return nil
 		})
 	}()
 	go func() {
 		defer wg.Done()
-		Add("test-2", 2, func() error {
+		Add("test-2", P2, func() error {
 			fmt.Println("test-2 bye-bye")
 			return errors.New("test-2 oh no")
 		})

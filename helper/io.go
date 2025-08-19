fd604834d7a05c53565d1fb8db2ebb8c3688e1f7
盛辉 <hui.sheng@sunmi.com>
2025-08-19 14:12:25 +0800

    Add LimitWriter

---
+++ helper/io.go
@@ -0,0 +1,30 @@
+package helper
+
+import "io"
+
+// LimitWriter returns a Writer that writes to w
+// but discards any bytes written beyond n bytes.
+// The underlying implementation is a *LimitedWriter.
+func LimitWriter(w io.Writer, n int64) io.Writer { return &LimitedWriter{w, n} }
+
+// A LimitedWriter writes to W but limits the amount of
+// data written to at most N bytes. Each call to Write
+// updates N to reflect the remaining write quota.
+// Write does not return an error when the limit is exceeded;
+// instead, it discards any bytes beyond N.
+type LimitedWriter struct {
+	W io.Writer // underlying writer
+	N int64     // max bytes remaining
+}
+
+func (l *LimitedWriter) Write(p []byte) (n int, err error) {
+	if l.N <= 0 {
+		return 0, nil
+	}
+	if int64(len(p)) > l.N {
+		p = p[:l.N]
+	}
+	n, err = l.W.Write(p)
+	l.N -= int64(n)
+	return
+}
---
+++ helper/io_test.go
@@ -0,0 +1,26 @@
+package helper
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLimitedWriter(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+
+	w1 := LimitWriter(buf, 10)
+	n, err := w1.Write([]byte("Hello"))
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello", buf.String())
+
+	buf.Reset()
+
+	w2 := LimitWriter(buf, 5)
+	n, err = w2.Write([]byte("Hello, world!"))
+	assert.Equal(t, 5, n)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello", buf.String())
+}

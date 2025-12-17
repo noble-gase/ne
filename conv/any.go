8ec1cfdfad6b125ff7a7d73dd9f4012e7bf33252
盛辉 <hui.sheng@sunmi.com>
2025-12-17 09:54:32 +0800

    Add AnyToStr

---
+++ conv/any.go
@@ -0,0 +1,63 @@
+package conv
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"strconv"
+)
+
+func AnyToStr(val any) string {
+	switch v := val.(type) {
+	case string:
+		return v
+	case bool:
+		return strconv.FormatBool(v)
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	case float32:
+		return strconv.FormatFloat(float64(v), 'f', -1, 32)
+	case int:
+		return strconv.Itoa(v)
+	case int8:
+		return strconv.FormatInt(int64(v), 10)
+	case int16:
+		return strconv.FormatInt(int64(v), 10)
+	case int32:
+		return strconv.FormatInt(int64(v), 10)
+	case int64:
+		return strconv.FormatInt(v, 10)
+	case uint:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint64:
+		return strconv.FormatUint(v, 10)
+	case json.Number:
+		return v.String()
+	case []byte:
+		return string(v)
+	case template.HTML:
+		return string(v)
+	case template.URL:
+		return string(v)
+	case template.JS:
+		return string(v)
+	case template.CSS:
+		return string(v)
+	case template.HTMLAttr:
+		return string(v)
+	case nil:
+		return "<nil>"
+	case error:
+		return v.Error()
+	case fmt.Stringer:
+		return v.String()
+	default:
+		return fmt.Sprintf("%+v", val)
+	}
+}

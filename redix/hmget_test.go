0d6f47e8d081acbf56095e2bc97239d7a1634949
盛辉 <hui.sheng@sunmi.com>
2026-01-28 11:09:23 +0800

    Update redix

--- redix/hmget_test.go
+++ redix/hmget_test.go
@@ -8,6 +8,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestHGetAll(t *testing.T) {
+	ctx := context.Background()
+
+	uc := redis.NewUniversalClient(&redis.UniversalOptions{
+		Addrs: []string{"127.0.0.1:6379"},
+		DB:    0,
+	})
+	uc.HMSet(ctx, "test", "foo", `{"id":1,"name":"foo"}`, "bar", `{"id":2,"name":"bar"}`, "hello", `{"id":3,"name":"hello"}`)
+	defer uc.Del(ctx, "test")
+
+	type Demo struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
+	ret, err := HGetAll[Demo](ctx, uc, "test")
+	assert.Nil(t, err)
+	t.Log(ret)
+}
+
 func TestHMGetMap(t *testing.T) {
 	ctx := context.Background()
 
@@ -34,29 +53,10 @@ func TestHMGetStringMap(t *testing.T) {
 		Addrs: []string{"127.0.0.1:6379"},
 		DB:    0,
 	})
-	uc.HMSet(ctx, "test", "foo", "field-foo", "bar", "field-bar", "hello", "field-hello")
-	defer uc.Del(ctx, "test")
-
-	ret, err := HMGetStringMap(ctx, uc, "test", []string{"foo", "bar", "hello", "none"})
-	assert.Nil(t, err)
-	t.Log(ret)
-}
-
-func TestHGetAll(t *testing.T) {
-	ctx := context.Background()
-
-	uc := redis.NewUniversalClient(&redis.UniversalOptions{
-		Addrs: []string{"127.0.0.1:6379"},
-		DB:    0,
-	})
 	uc.HMSet(ctx, "test", "foo", `{"id":1,"name":"foo"}`, "bar", `{"id":2,"name":"bar"}`, "hello", `{"id":3,"name":"hello"}`)
 	defer uc.Del(ctx, "test")
 
-	type Demo struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	}
-	ret, err := HGetAll[Demo](ctx, uc, "test")
+	ret, err := HMGetStringMap(ctx, uc, "test", []string{"foo", "bar", "hello", "none"})
 	assert.Nil(t, err)
 	t.Log(ret)
 }
--- redix/mget_test.go
+++ redix/mget_test.go
@@ -34,7 +34,7 @@ func TestMGetStringMap(t *testing.T) {
 		Addrs: []string{"127.0.0.1:6379"},
 		DB:    0,
 	})
-	uc.MSet(ctx, "foo", "test-foo", "bar", "test-bar", "hello", "test-hello")
+	uc.MSet(ctx, "foo", `{"id":1,"name":"foo"}`, "bar", `{"id":2,"name":"bar"}`, "hello", `{"id":3,"name":"hello"}`)
 	defer uc.Del(ctx, "foo", "bar", "hello")
 
 	ret, err := MGetStringMap(ctx, uc, []string{"foo", "bar", "hello", "none"})

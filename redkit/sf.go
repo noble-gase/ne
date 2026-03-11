252297cc848a70cb0d05b12b1b5798c9d73d5819
盛辉 <hui.sheng@sunmi.com>
2026-03-11 14:28:22 +0800

    Update config

--- redkit/sf.go
+++ redkit/sf.go
@@ -17,3 +17,37 @@ if redis.call('TTL', KEYS[1]) == -1 then
     redis.call('EXPIRE', KEYS[1], ARGV[3])
 end
 `)
+
+type Config struct {
+	// Addrs 地址
+	Addrs []string `json:"addrs" mapstructure:"addrs"`
+	// Options 选项
+	Options Options `json:"options" mapstructure:"options"`
+}
+
+type Options struct {
+	// Username 用户名
+	Username string `json:"username" mapstructure:"username"`
+	// Password 密码
+	Password string `json:"password" mapstructure:"password"`
+	// DialTimeout 连接超时时间（单位：秒）
+	DialTimeout int `json:"dial_timeout" mapstructure:"dial_timeout"`
+	// ReadTimeout 读取超时时间（单位：秒）
+	ReadTimeout int `json:"read_timeout" mapstructure:"read_timeout"`
+	// WriteTimeout 写入超时时间（单位：秒）
+	WriteTimeout int `json:"write_timeout" mapstructure:"write_timeout"`
+	// PoolSize 连接池大小
+	PoolSize int `json:"pool_size" mapstructure:"pool_size"`
+	// PoolTimeout 连接池超时时间（单位：秒）
+	PoolTimeout int `json:"pool_timeout" mapstructure:"pool_timeout"`
+	// MinIdleConns 最小空闲连接数
+	MinIdleConns int `json:"min_idle_conns" mapstructure:"min_idle_conns"`
+	// MaxIdleConns 最大空闲连接数
+	MaxIdleConns int `json:"max_idle_conns" mapstructure:"max_idle_conns"`
+	// MaxActiveConns 最大活跃连接数
+	MaxActiveConns int `json:"max_active_conns" mapstructure:"max_active_conns"`
+	// ConnMaxIdleTime 连接最大闲置时间（单位：秒）
+	ConnMaxIdleTime int `json:"conn_max_idle_time" mapstructure:"conn_max_idle_time"`
+	// ConnMaxLifetime 连接最大生命时长（单位：秒）
+	ConnMaxLifetime int `json:"conn_max_lifetime" mapstructure:"conn_max_lifetime"`
+}
--- sqlkit/db.go
+++ sqlkit/db.go
@@ -16,21 +16,26 @@ type Config struct {
 	//  [-MySQL] mysql
 	//  [-PgSQL] pgx
 	//  [SQLite] sqlite3
-	Driver string
+	Driver string `json:"driver" mapstructure:"driver"`
 	// DSN 数据源名称
 	//
 	//  [-MySQL] <username>:<password>@tcp(<host>:3306)/<db>?timeout=10s&charset=utf8mb4&parseTime=True&loc=Local
 	//  [-PgSQL] postgres://<username>:<password>@<host>:5432/<dbname>
 	//  [SQLite] file::memory:?cache=shared || file:</path/test.db>
-	DSN string
+	DSN string `json:"dsn" mapstructure:"dsn"`
+	// Options 数据库连接池选项
+	Options Options `json:"options" mapstructure:"options"`
+}
+
+type Options struct {
 	// MaxOpenConns 设置最大可打开的连接数
-	MaxOpenConns int
+	MaxOpenConns int `json:"max_open_conns" mapstructure:"max_open_conns"`
 	// MaxIdleConns 连接池最大闲置连接数
-	MaxIdleConns int
-	// ConnMaxIdleTime 连接最大闲置时间
-	ConnMaxIdleTime time.Duration
-	// ConnMaxLifetime 连接的最大生命时长
-	ConnMaxLifetime time.Duration
+	MaxIdleConns int `json:"max_idle_conns" mapstructure:"max_idle_conns"`
+	// ConnMaxIdleTime 连接最大闲置时间（单位：秒）
+	ConnMaxIdleTime int `json:"conn_max_idle_time" mapstructure:"conn_max_idle_time"`
+	// ConnMaxLifetime 连接的最大生命时长（单位：秒）
+	ConnMaxLifetime int `json:"conn_max_lifetime" mapstructure:"conn_max_lifetime"`
 }
 
 // NewDB returns a new sql.DB
@@ -44,10 +49,10 @@ func NewDB(cfg *Config) (*sql.DB, error) {
 		return nil, err
 	}
 
-	db.SetMaxOpenConns(cfg.MaxOpenConns)
-	db.SetMaxIdleConns(cfg.MaxIdleConns)
-	db.SetConnMaxIdleTime(cfg.ConnMaxIdleTime)
-	db.SetConnMaxLifetime(cfg.ConnMaxLifetime)
+	db.SetMaxOpenConns(cfg.Options.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.Options.MaxIdleConns)
+	db.SetConnMaxIdleTime(time.Duration(cfg.Options.ConnMaxIdleTime) * time.Second)
+	db.SetConnMaxLifetime(time.Duration(cfg.Options.ConnMaxLifetime) * time.Second)
 
 	return db, nil
 }
